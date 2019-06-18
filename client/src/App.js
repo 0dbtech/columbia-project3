571bbf475f0b2dca2e5da55b0a1ed70077b814d2
Brian Smith <bsmith@dyn-209-2-209-217.dyn.columbia.edu>
2019-06-17 21:10:46 -0400

    name change

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Pupster Heroku Deployment Template</h2>
+          <h2>Paw Pal | Heroku Deployment Template</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>client/src/App.js</code> and save to reload.  
