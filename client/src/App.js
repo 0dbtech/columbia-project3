a49915c86ae5eb4156f21116e21946118ad78c92
Brian Smith <bsmith@dyn-209-2-208-75.dyn.columbia.edu>
2019-06-19 20:54:05 -0400

    search component post merge test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import SearchComponent from "./components/Search";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>client/src/App.js</code> and save to reload.  
         </p>
+        <SearchComponent></SearchComponent>
       </div>
     );
   }
