654b6337f16666a261cbccb0aa2c608b73f7d831
Chris Warner <christopher.warner92@gmail.com>
2019-07-03 15:39:41 -0400

    shelters now get retrieved and stored in state in home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,9 +2,23 @@ import React from 'react';
 import HeaderComponent from "../components/Header/header";
 import FooterComponent from "../components/Footer/footer";
 import SearchComponent from "../components/Search/search";
-
+import axios from "axios";
 class Home extends React.Component {
   //ToDo add constructor, state, component did mount and axios function to store shelters in state.
+  constructor(props) {
+    super(props);
+    this.state = { 
+      shelters: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/all').then(res => {
+      console.log(res);
+      this.setState({shelters:res.data.shelter_res});
+      console.log(this.state.shelters)
+    });
+  }
 
   render() {
     return (
