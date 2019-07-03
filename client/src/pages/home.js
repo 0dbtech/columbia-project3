29d7950c1f4f94ad4ac0f6607956370861ed0746
Chris Warner <christopher.warner92@gmail.com>
2019-07-03 15:34:44 -0400

    added todo in home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,6 +4,8 @@ import FooterComponent from "../components/Footer/footer";
 import SearchComponent from "../components/Search/search";
 
 class Home extends React.Component {
+  //ToDo add constructor, state, component did mount and axios function to store shelters in state.
+
   render() {
     return (
       <div className="home-page">
