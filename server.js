4422494f848c6ada2bd987dc6cce72c5a21c0019
Chris Warner <christopher.warner92@gmail.com>
2019-07-18 14:47:38 -0400

    ready

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 // const { Dog, Shelter, Tag, User } = require('./sequelize');
 const passport = require('passport'); 
@@ -21,6 +21,10 @@ app.use(passport.session());
 
 require('./routes/api-routes')(app);
 // require('./routes/html-routes')(app);
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
