43016f366f99448f73764f2417bbd5536b33e444
Chris Warner <christopher.warner92@gmail.com>
2019-07-20 10:26:39 -0400

    heroku deploy

---
+++ client/build/precache-manifest.176f20db6c212862607288f800899033.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "490711b7a3ddbc06c81c",
+    "url": "/static/css/main.6131abc1.chunk.css"
+  },
+  {
+    "revision": "490711b7a3ddbc06c81c",
+    "url": "/static/js/main.60d36ca7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "297912017c4b31cf6e21",
+    "url": "/static/css/2.c402a0dd.chunk.css"
+  },
+  {
+    "revision": "297912017c4b31cf6e21",
+    "url": "/static/js/2.9e22ba60.chunk.js"
+  },
+  {
+    "revision": "65fca36e247ba03807a95a76cf3be116",
+    "url": "/static/media/0.65fca36e.jpg"
+  },
+  {
+    "revision": "57d20dba6b48cf64dcd9de4f92308f53",
+    "url": "/static/media/3.57d20dba.jpg"
+  },
+  {
+    "revision": "c8cb601bad2de5dfe5db6f659ba968d4",
+    "url": "/static/media/8.c8cb601b.jpg"
+  },
+  {
+    "revision": "820b59cd6ee6b18a9ecc3f4ade5f2640",
+    "url": "/static/media/1.820b59cd.jpg"
+  },
+  {
+    "revision": "271460f8df23069b0dad48138259fb37",
+    "url": "/static/media/99.271460f8.png"
+  },
+  {
+    "revision": "3eb6c848213491a44d6aecab481cc333",
+    "url": "/static/media/65.3eb6c848.jpg"
+  },
+  {
+    "revision": "e622303ad1efba8e32d95c55f5e12899",
+    "url": "/static/media/01.e622303a.jpg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "ff01205bf735d92ab085146e25071ee5",
+    "url": "/index.html"
+  }
+];
